993058ce9d935c76e5fd944d84bd68da166664d0
Forrust <info@forrust.com>
2024-04-07 10:30:12 +0200

    started with anchor

--- programs/solana/src/lib.rs
+++ programs/solana/src/lib.rs
@@ -1,15 +1,68 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_program;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("AckSBrz9UShZJdzKBXAeUP9jX7AX9MPdR9RGrvzGwoXb");
 
 #[program]
 pub mod solana {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn store_crime(ctx: Context<StoreCrime>, outcome: String, reward: u64) -> Result<()> {
+        let crime: &mut Account<Crime> = &mut ctx.accounts.crime;
+        let author: &Signer = &ctx.accounts.author;        
+        let clock: Clock = Clock::get().unwrap();
+
+        if outcome.len() > 10 {
+            return Err(ErrorCode::OutcomeTooLong.into());
+        }
+
+        if reward > 100 {
+            return Err(ErrorCode::RewardTooHigh.into());
+        }
+
+        crime.author = *author.key;
+        crime.timestamp = clock.unix_timestamp;
+        crime.outcome = outcome.to_string();
+        crime.reward = reward;
+        
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StoreCrime<'info>{
+    #[account(init, payer = author, space = Crime::LEN)]
+    pub crime: Account<'info, Crime>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    /// CHECK: This is a system program account, and its address is known and fixed.
+    #[account(address = system_program::ID)]
+    pub system_program: AccountInfo<'info>,
+}
+
+#[account]
+pub struct Crime {
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub outcome: String,
+    pub reward: u64
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4; // Stores the size of the string.
+const STRING_OUTCOME: usize = 10 * 4; // 10 chars max.
+const U64_LENGTH: usize = 8;
+
+impl Crime {
+    const LEN: usize = DISCRIMINATOR_LENGTH + PUBLIC_KEY_LENGTH + TIMESTAMP_LENGTH + STRING_LENGTH_PREFIX + STRING_OUTCOME + U64_LENGTH;
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Outcome too long")]
+    OutcomeTooLong,
+    #[msg("Reward too high")]
+    RewardTooHigh
+}
--- tests/solana.ts
+++ tests/solana.ts
@@ -1,6 +1,8 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Solana } from "../target/types/solana";
+import * as anchor from '@project-serum/anchor';
+import { Program, BN } from '@project-serum/anchor';
+import { Solana } from '../target/types/solana';
+import * as assert from "assert";
+import * as bs58 from "bs58";
 
 describe("solana", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +10,43 @@ describe("solana", () => {
 
   const program = anchor.workspace.Solana as Program<Solana>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it('can store an outcome and reward', async () => {
+    const outcome = 'Success';
+    const reward = new BN(1);
+    const crime = anchor.web3.Keypair.generate();
+    const accounts = {
+      crime: crime.publicKey,
+      author: program.provider.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    };
+    await program.methods.storeCrime(outcome, reward).accounts(accounts).signers([crime]).rpc();
+
+    const crimeAccount = await program.account.crime.fetch(crime.publicKey);
+
+    assert.ok(crimeAccount.outcome === outcome);
+    assert.ok(crimeAccount.reward.eq(reward));
+    assert.ok(crimeAccount.author.equals(program.provider.publicKey));
+    assert.ok(crimeAccount.timestamp);
   });
+
+  it('can fetch all crimes', async () => {
+    const crimeAccounts = await program.account.crime.all();
+    console.log(crimeAccounts);
+    assert.equal(crimeAccounts.length, 1);
+  });
+
+  it('can filter crimes by author', async () => {
+    const authorPublicKey = program.provider.publicKey
+    const tweetAccounts = await program.account.crime.all([
+      {
+        memcmp: {
+          offset: 8, // Discriminator.
+          bytes: authorPublicKey.toBase58(),
+        }
+      }
+    ]);
+
+    assert.equal(tweetAccounts.length, 1);
+  });
+
 });
